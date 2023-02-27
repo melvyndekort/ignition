f7903f7d9cc3ea237722457bc8c7ede0fa26799d
Melvyn de Kort <melvyn@mdekort.nl>
2023-02-27 20:21:33 +0100

    Working lmserver setup

--- scripts/prepare-lmserver.sh
+++ scripts/prepare-lmserver.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-# use https://ignition.mdekort.nl/install-lmserver.ign
+[ "$#" -ne 1 ] && echo "One parameter expected: device" && exit 1
+
+DEVICE="$1"
+
+(
+  mkdir -p temp
+  cd temp
+  curl -sLO https://ignition.mdekort.nl/lmserver.ign
+)
+
+docker run \
+  --privileged \
+  --rm \
+  -v /dev:/dev \
+  -v /run/udev:/run/udev \
+  -v ./temp:/data \
+  -w /data \
+  quay.io/coreos/coreos-installer:release \
+  install "$DEVICE" -i lmserver.ign
+
+rm -rf temp
--- scripts/prepare-pihole.sh
+++ scripts/prepare-pihole.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-# use https://ignition.mdekort.nl/install-pihole.ign
+#
+# TODO: This script needs to be modified for Raspberry Pi
+#
+exit 1
+
+
+[ "$#" -ne 1 ] && echo "One parameter expected: device" && exit 1
+
+DEVICE="$1"
+
+(
+  mkdir -p temp
+  cd temp
+  curl -sLO https://ignition.mdekort.nl/pihole.ign
+)
+
+docker run \
+  --privileged \
+  --rm \
+  -v /dev:/dev \
+  -v /run/udev:/run/udev \
+  -v ./temp:/data \
+  -w /data \
+  quay.io/coreos/coreos-installer:release \
+  install "$DEVICE" -i pihole.ign
+
+rm -rf temp
